da34ab3d32fa8aeccf6f9f02be3b989dd5e99f7d
Ambar Sethi <ambarrrsethi@gmail.com>
2015-10-13 09:25:34 +0100

    hw commit

--- controller/gameController.js
+++ controller/gameController.js
@@ -1,11 +1,55 @@
 angular.module('gameApp').controller('GameController', GameController);
 
 function GameController(){
-  this.X = 0;
-  this.O = 1;
+  this.name = "Spock or Peace";
+  this.board = [1, 0];
+  this.newMove;
+  this.playerMoves = playerMoves;
 
-  function firstMove(){
+  function playerMoves(){
+    console.log(this.newMove)
+    // if(this.newMove == null){
+    //   this.board.push(this.newMove);
+    //   console.log(this.board.push(this.newMove))
+    // }
+    if(this.newMove % 2 == 0){
+      // this.board.push(this.newMove);
+      this.newMove = 'o'
+      // console.log(newMove)
 
-  }
+    } else if (this.newMove % 2 != 0) {
+      this.board.push(this.newMove);    
+      this.newMove = 'x' 
+    }
+    
+  };
+
+
+
+
+
+
+
+
+///When you click a square either put in x or o depending on the player maybe judge the player based on who went first
+//if player %! = 0 then character = X
+//else player % = 0 then character = 0 
+
+//first logic function
+//if pla
+
+// function playerMove(){
+//   if (player == 0){
+//     //add X
+//     this.player='X'
+//   } else {
+//     //add 0 
+//     this.player='O'
+
+//   }
+// }
+
+
+
 
 };
